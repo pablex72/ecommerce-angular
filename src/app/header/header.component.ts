8d94a8b818f0911114400291d02df772eb8482fc
pablo <tonyp2hoy@gmail.com>
2025-06-07 02:22:47 -0400

    adicionando css puro

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-header",
     templateUrl: './header.component.html',
-    styles: ['a{text-decoration: none; margin: 0 10px;}', 'button{padding: 10px 20px;}', '.ekart--header {width: 100%; height: 70px}']
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
 
