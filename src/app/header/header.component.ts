9dd9dbc6570d56dad51c4d06ca77971fc9b98c0f
pablo <tonyp2hoy@gmail.com>
2025-06-06 23:59:09 -0400

    add: header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-header",
+    templateUrl: './header.component.html',
+    styles: ['a{text-decoration: none; margin: 0 10px;}', 'button{padding: 10px 20px;}', '.ekart--header {width: 100%; height: 70px}']
+})
+export class HeaderComponent{
+
+} 
