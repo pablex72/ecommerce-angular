828ad4e6de0d9827797ff8810a3f081ce97e6cc8
pablo <tonyp2hoy@gmail.com>
2025-06-17 00:34:19 -0400

    adicionando event binding for button

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  name = "Pedro P";
+  // name = "Pedro P";
 
+  addToCart:number = 0;
   product = {
     name: 'iPhone',
     price: 789,
@@ -19,5 +20,23 @@ export class ProductListComponent {
 
   getDiscountedPrice(){
     return this.product.price - (this.product.price * this.product.discount) / 100
+  }
+
+  onNameChange(event: any){
+    // this.name = event.target.value;
+    // console.log(event.target.value)
+    // this.name = "Mark";
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--; 
+    }
+  }
+
+  incrementCartValue(){
+    if(this.addToCart <= this.product.inStock){
+      this.addToCart++;
+    }
   }
 }
