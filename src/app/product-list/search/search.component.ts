33b751ff1a067bd6e4e94b0390155d26aebbe476
pablo <tonyp2hoy@gmail.com>
2025-06-17 21:07:17 -0400

    opcion simple data binding

---
+++ src/app/product-list/search/search.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+
+  searchText: string = 'Awwwww';
+
+  updateSearchText(event: any){
+    this.searchText = event.target.value;
+  }
+}
